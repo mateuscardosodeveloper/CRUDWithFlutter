df4456fd8485e59280b68cc5a3d0039055029cb6
raiizoor <mateus.informatica44@gmail.com>
2021-07-14 20:45:34 -0300

    definindo formulário

--- lib/app/views/contact_form.dart
+++ lib/app/views/contact_form.dart
@@ -1,13 +1,75 @@
+import 'package:crud_with_flutter/app/views/contact_form_back.dart';
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class ContactForm extends StatelessWidget {
 
+  Widget fieldName(ContactFormBack back) {
+    return TextFormField (  
+      initialValue: back.contact.nome,
+      decoration: InputDecoration ( 
+        labelText: 'Nome:'
+      )
+    );
+  }
 
-  @override
+  Widget fieldEmail(ContactFormBack back) {
+    return TextFormField (  
+      initialValue: back.contact.email,
+      decoration: InputDecoration ( 
+        labelText: 'E-mail:'
+      )
+    );
+  }
+
+  Widget fieldPhone(ContactFormBack back) {
+    var mask = MaskTextInputFormatter(mask: '(##) # ####-####');
+    return TextFormField (  
+      initialValue: back.contact.telefone,
+      inputFormatters: [mask],
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration ( 
+        labelText: 'Telefone:', 
+        hintText: '(99) 9 9999-9999'
+      )
+    );
+  }
+
+  Widget fieldURLImage(ContactFormBack back) {
+    return TextFormField (  
+      initialValue: back.contact.urlAvatar,
+      decoration: InputDecoration ( 
+        labelText: 'Endereço Foto:', 
+        hintText: 'http://www.site.com'
+      )
+    );
+  }
+
+
+   @override
   Widget build(BuildContext context) {
+    var _back = ContactFormBack(context);
     return Scaffold(
-      appBar: AppBar(title: Text('Cadastro de Contato'),),
-      
+      appBar: AppBar(  
+        title: Text('Cadastro de Contato'),
+        actions: [
+          IconButton(icon: Icon(Icons.save), onPressed: null)
+        ],
+      ),
+      body: Padding(  
+        padding: EdgeInsets.all(10),
+        child: Form(  
+          child:  Column (
+            children: [
+              fieldName(_back),
+              fieldEmail(_back),
+              fieldPhone(_back), 
+              fieldURLImage(_back)
+            ],
+            ),
+        ),
+      ),
+
     );
   }
 }
--- lib/app/views/contact_list.dart
+++ lib/app/views/contact_list.dart
@@ -10,11 +10,9 @@ class ContactList extends StatelessWidget {
   final _back = ContactListBack();
 
   CircleAvatar circleAvatar(String url){
-    try{
-      return CircleAvatar(backgroundImage: NetworkImage(url));
-    }catch(e){
-      return CircleAvatar(child: Icon(Icons.person));
-    }
+    return(Uri.tryParse(url).isAbsolute)?
+      CircleAvatar(backgroundImage: NetworkImage(url))
+      : CircleAvatar(child: Icon(Icons.person));
   }
 
   Widget iconEditButton(Function onPressed){
